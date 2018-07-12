b682521a1743fbc1675f253dabbe3a7537471a23
grygon <grygohn@gmail.com>
2018-07-12 15:45:37 -0400

    Better secret

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const secret = require('./secret.js');
+const secret = require('./secret.json');
+
+const prefix = '~';
 
 client.on('ready', () => {
     console.log('Ready!');
 });
+
+client.on('message', message => {
+	if (message.author.bot) return;
+	console.log(message.content);
+	if (message.content.startsWith('${prefix}ping')) {
+		message.channel.send('Pong!');
+	}
+})
 
 console.log('Connecting...');
 client.login(secret.token);
