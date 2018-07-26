b1b3e4b1a10a76e2a5f7eb891667946d61b4bd89
grygon <grygohn@gmail.com>
2018-07-26 08:24:03 -0400

    Fixed playback (probably)

--- commands/play.js
+++ commands/play.js
@@ -11,7 +11,7 @@ module.exports = {
     	// TODO: Handle (and differentiate between) files, standard media links, and youtube links. Maybe split off if necessary.
 
     	// Find matching sound files
-        const soundFiles = fs.readdirSync(`./sounds/${message.guild.id}`).filter(f => f.startsWith(args[0] + ".").filter(f => !f.endsWith(".json")));
+        const soundFiles = fs.readdirSync(`./sounds/${message.guild.id}`).filter(f => f.startsWith(args[0] + ".")).filter(f => !f.endsWith(".json"));
         if (soundFiles.length === 1) {
         	// Join the channel if possible
 			if (message.member.voiceChannel) {
@@ -19,7 +19,7 @@ module.exports = {
 		        .then(connection => { // Connection is an instance of VoiceConnection
 	        	  // Play found sound file
 	        	  console.log(`Playing ${args[0]} from file ${soundFiles[0]}`);
-	        	  const dispatcher = connection.playFile(`./sounds/${message.guild.id}/${soundFiles[0]}`);
+	        	  const dispatcher = connection.playFile(`./sounds/${message.guild.id}/${soundFiles[0]}`,{passes: 2});
 				  message.reply("Playing " + args[0] + "...");
 		        });
 		    } else {
@@ -27,7 +27,7 @@ module.exports = {
 		    }
 
         // Failures below
-        } else if (soundFiles.length === 0) {
+        } else if (soundFiles.length === 0) {\
         	message.reply("Don\'t have that sound registered!");
         } else {
         	throw "Too many sound files for that name exist!";
